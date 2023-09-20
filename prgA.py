e0edac659ba9899ba0dcddca4dff954fe6abde7a
Gokilapriya-S <144409639+Gokilapriya-S@users.noreply.github.com>
2023-09-20 13:06:14 +0530

    Add files via upload

---
+++ prgA.py
@@ -0,0 +1,16 @@
+my_age = int(input("Enter my age:"))
+your_age = int(input("Enter your age:")) 
+if my_age > your_age:
+ if my_age-your_age == 1: 
+  
+     print("I am a year older than you.")
+else:
+     print("I am",my_age-your_age,"year older than you.")
+
+if your_age > my_age:
+ if your_age-my_age == 1: 
+  
+     print("I am a year older than you.")
+else:
+     print("I am",your_age-my_age,"year older than you.")
+
